e644edb3e6c99260fa21bfe5bd22e133d647a9a1
vpsines <sasip6028@gmail.com>
2025-11-01 01:14:18 +0530

    fix:correct utility class

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -16,7 +16,7 @@ const TradingViewWidget = ({title,scriptUrl,config,height =600,className}:Tradin
 
 
     return (
-        <div className="w-ful">
+        <div className="w-full">
             {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
         <div className={cn("tradingview-widget-container",className)} ref={containerRef} >
             <div className="tradingview-widget-container__widget" style={{  height, width: "100%" }}/>
